802a1311332f1c8a7e509d9d0c604858d35af30b
nadzimalrash <hello@nadzim.com>
2023-12-03 23:25:15 +0800

    rebuild public dir

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
-    eleventyConfig.addPassthroughCopy('./src/script.js');
     eleventyConfig.addPassthroughCopy('./src/assets');
 
     return {
