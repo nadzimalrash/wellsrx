e58d00cdd180042d939669955d4ab02d18f0c428
nadzimalrash <hello@nadzim.com>
2023-12-03 20:32:53 +0800

    home page and template

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/script.js');
     eleventyConfig.addPassthroughCopy('./src/assets');
 
     return {
